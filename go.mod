2e262333b89bd53640a646496bb3acc8c4677b74
akari <akariondev@gmail.com>
2023-02-03 11:53:27 -0300

    httpbinip update

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/AkariOficial/httpbinip
 
 go 1.19
-
-require github.com/akarioficial/httpbinip v0.5.1 // indirect
--- main.go
+++ main.go
@@ -3,20 +3,18 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-
-	"github.com/AkariOficial/httpbinip/ip/httpbinip"
+	"github.com/AkariOficial/httpbinip/ip"
 )
 
 
 func main() {
-    ip, err := httpbinip.GetIp()
+    ip, err := httpbin.GetIp()
 
     if err != nil {
         fmt.Println("Error getting IP:", err)
     }
 
-    err = ioutil.WriteFile("lista.txt", []byte(ip), 0644)
-    
+    err = ioutil.WriteFile("lista.txt", []byte(ip), 0644) 
     
 	if err != nil {
 		fmt.Println("Error writing to file:", err)
